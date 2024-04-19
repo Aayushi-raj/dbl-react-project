52fba44665dc1ca0c6fba821f5106bcd464d7992
Aayushi123raj <aayushi09raj@gmail.com>
2024-04-19 22:19:30 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BsPerson} from 'react'
+
+const Navbar=()=>{
+    return (
+        <div>
+            <div>
+                <h1>BEACHES.</h1>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>Travel</li>
+                <li>View</li>
+                <li>Book</li>
+
+                
+            </ul>
+         
+        </div>
+    )
+}
+export default Navbar
