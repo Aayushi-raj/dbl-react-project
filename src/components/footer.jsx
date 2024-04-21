4b5d13e8145def094b65e28f48c1faa72430252f
Aayushi123raj <aayushi09raj@gmail.com>
2024-04-21 21:24:23 +0530

    few changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <div className='w-full bg-gray-100 py-16'>
       <div className='max-w-[1240px] mx-auto flex flex-col px-4'>
         <div className='sm:flex text-center justify-between items-center'>
-          <h1>BEACHES.</h1>
+          <h1>BEACHES</h1>
           <div className='flex justify-between w-full sm:max-w-[280px] my-4'>
             <FaFacebook className='icon' />
             <FaTwitter className='icon' />
@@ -23,18 +23,18 @@ const Footer = () => {
         </div>
         <div className='flex justify-between'>
           <ul className='lg:flex'>
-            <li>About</li>
-            <li>Partnerships</li>
-            <li>Careers</li>
-            <li>Newsroom</li>
-            <li>Advertising</li>
+            <li>ABOUT</li>
+            <li>PARTNERSHIPS</li>
+            <li>CAREERS</li>
+            <li>NEWSROOM</li>
+            <li>ADVERTISING</li>
           </ul>
           <ul className='text-right lg:flex'>
-            <li>Home</li>
-            <li>Destinations</li>
-            <li>Travel</li>
-            <li>View</li>
-            <li>Book</li>
+            <li>HOME</li>
+            <li>DESTINATONS</li>
+            <li>TRAVEL</li>
+            <li>VIEW</li>
+            <li>BOOK</li>
           </ul>
         </div>
       </div>
